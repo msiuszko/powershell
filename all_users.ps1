07547a3ab54d0590eb8fa96db9785a40158c9c61
Gosia <gosia@github>
2022-12-04 14:53:21 +0100

    Script to export all accounts

---
+++ all_users.ps1
@@ -0,0 +1,2 @@
+Get-ADUser -Filter * -SearchBase 'OU=po, OU=qwe, DC=qaz, DC=ab' | Where-Object { $_.DistinguishedName -notlike '*OU=Nieprzypisani*' } | Select Name |
+Export-Csv "c:\AD_nask_sa.csv" -Encoding UTF8 -NoTypeInformation
